c0e24f448bc369a9d035d295fca7fc3e0f21f64e
Victoria Ransom <victoriamransom@gmail.com>
2023-09-18 15:40:11 -0700

    update

--- polymorphism.py
+++ polymorphism.py
@@ -1,22 +1,28 @@
 class School: #parent class
     name="San Diego State University" #parent attribute
     city="San Diego" #parent attribute
+    username="user@school.com"
+    password="password3"
 
-    def info(self): #parent method
-        msg="\nThis school is: {}\nLocated in: {}".format(self.name,self.city)
-        return msg
+    def LoginInfo(self): #parent method
+        enter_username=input("Enter username: ")
+        enter_password=input("Enter password: ")
+        if (enter_username==self.username and enter_password==self.password):
+            print("Welcome, {}!".format(enter_username))
+        else:
+            print("Password or email is incorrect, please re-enter")
 
 class Professor(School): #child class1
     professor_name="Dr. Smith" #child attribute
     teaches="Psychology" #child attribute
     username="professor@school.com"
-    password="password"
+    pincode="1234"
 
-    def ProfessorLogin(self): # child1 polymorphism
-        prof_username=input("Enter username: ")
-        prof_password=input("Enter password: ")
-        if (prof_username==self.username and prof_password==self.password):
-            print("Welcome, {}!".format(professor_name))
+    def LoginInfo(self): # child1 polymorphism
+        enter_username=input("Enter username: ")
+        enter_pincode=input("Enter pincode: ")
+        if (enter_username==self.username and enter_pincode==self.pincode):
+            print("Welcome, {}!".format(enter_username))
         else:
             print("Password or email is incorrect, please re-enter")
         
@@ -24,19 +30,31 @@ class Student(School): #child class2
     student_name="Victoria Ransom" #child attribute
     major="Business" #child attribute
     active_student=False #child attribute
-    susername="victoriamransom@gmail.com"
-    spassword="victoria123"
+    username="victoriamransom@gmail.com"
+    password="victoria123"
+    DOB="12-4-1994"
 
-    def StudentLogin(self): #child2polymorphism
-        student_username=input("Enter username: ")
-        student_password=input("Enter password: ")
-        if (student_username==self.susername and student_password==self.spassword):
-            print("Welcome, {}!".format(student_name))
+    def LoginInfo(self): #child2polymorphism
+        enter_username=input("Enter username: ")
+        enter_password=input("Enter password: ")
+        enter_DOB=input("Enter DOB: ")
+        if (enter_username==self.username and enter_password==self.password and enter_DOB==self.DOB):
+            print("Welcome, {}!".format(enter_username))
         else:
             print("Password or email is incorrect, please re-enter")
 
 
+admin=School()
+print(admin.LoginInfo())
+
 professor=Professor()
-print(professor.ProfessorLogin())
+print(professor.LoginInfo())
+
+student=Student()
+print(student.LoginInfo())
+
+
+
+
     
     
