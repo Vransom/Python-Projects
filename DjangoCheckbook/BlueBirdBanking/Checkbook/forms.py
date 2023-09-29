79ffbdc27d5bebdd0bb6a449e9aab03adf668330
Victoria Ransom <victoriamransom@gmail.com>
2023-09-29 10:26:50 -0700

    Django Checkbook

---
+++ DjangoCheckbook/BlueBirdBanking/Checkbook/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import Account, Transaction
+
+#creates account form based on Account Model
+class AccountForm(ModelForm):
+    class Meta:
+        model = Account
+        fields= '__all__'
+
+
+#creates transaction form based on Transaction Model
+class TransactionForm(ModelForm):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
